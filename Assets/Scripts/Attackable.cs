4aac655e32ccbdf36d3e68e6aea5ccd66cc2b093
Nikolay Danailov <frostblooded@yahoo.com>
2019-08-07 15:42:34 +0300

    Make enemies attackable

---
+++ Assets/Scripts/Attackable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attackable : MonoBehaviour
+{
+    public float health;
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+
+        if(health < 0.0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
