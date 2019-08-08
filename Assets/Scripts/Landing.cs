ce386e8ea599bf92283cac832c8e8539d3802186
Nikolay Danailov <frostblooded@yahoo.com>
2019-08-08 19:49:36 +0300

    Make towers spawn on unoccupied landings.

---
+++ Assets/Scripts/Landing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landing : MonoBehaviour
+{
+    public GameObject towerPrefab;
+    public bool occupied;
+
+    public void SpawnTower()
+    {
+        Instantiate(towerPrefab, transform);
+        occupied = true;
+    }
+}
---
+++ Assets/Scripts/TowerSpawner.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSpawner : MonoBehaviour
+{
+    public float spawnCooldown;
+
+    Transform landingsHolder;
+    float lastSpawnTime;
+
+    private void Start()
+    {
+        landingsHolder = GameObject.Find("Landings").transform;
+        lastSpawnTime = Mathf.NegativeInfinity;
+    }
+
+    private void Update()
+    {
+        if (lastSpawnTime + spawnCooldown <= Time.time)
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        Landing randomUnoccupiedLanding = GetRandomUnoccupiedLanding();
+        
+        if(randomUnoccupiedLanding)
+        {
+            randomUnoccupiedLanding.SpawnTower();
+            lastSpawnTime = Time.time;
+        }
+    }
+
+    private Landing GetRandomUnoccupiedLanding()
+    {
+        List<Landing> unoccupiedLandings = GetUnoccupiedLandings();
+
+        if(unoccupiedLandings.Count == 0)
+        {
+            return null;
+        }
+
+        return unoccupiedLandings[Random.Range(0, unoccupiedLandings.Count)];
+    }
+
+    private List<Landing> GetUnoccupiedLandings()
+    {
+        var result = new List<Landing>();
+
+        foreach (Transform landingObject in landingsHolder)
+        {
+            Landing landing = landingObject.GetComponent<Landing>();
+
+            if(!landing.occupied)
+            {
+                result.Add(landing);
+            }
+        }
+
+        return result;
+    }
+}
