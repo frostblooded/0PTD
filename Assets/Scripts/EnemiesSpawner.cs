7e58ac75a7027d0edcccd4d56fa1c4e2c423e21a
Nikolay Danailov <frostblooded@yahoo.com>
2019-08-08 13:38:25 +0300

    Rename EnemySpawner to EnemiesSpawner.

--- Assets/Scripts/EnemiesSpawner.cs
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
+public class EnemiesSpawner : MonoBehaviour
 {
     public float enemySpawnCooldown;
     public GameObject enemyPrefab;
