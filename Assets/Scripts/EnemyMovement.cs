33f01d5dda0d59029a1f96aaef1b687391d86b5f
Nikolay Danailov <frostblooded@yahoo.com>
2019-08-08 13:23:51 +0300

    Make enemies spawn and move

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public NavMeshAgent navMesgAgent;
+
+    Transform enemiesGoal;
+
+    private void Start()
+    {
+        enemiesGoal = GameObject.Find("EnemiesGoal").transform;
+        navMesgAgent.SetDestination(enemiesGoal.position);
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float enemySpawnCooldown;
+    public GameObject enemyPrefab;
+
+    float lastSpawnTime;
+    GameObject enemiesHolder;
+
+    private void Start()
+    {
+        lastSpawnTime = Mathf.NegativeInfinity;
+        enemiesHolder = GameObject.Find("Enemies");
+    }
+
+    private void Update()
+    {
+        if(lastSpawnTime + enemySpawnCooldown < Time.time)
+        {
+            Spawn();
+        }
+    }
+
+    public void Spawn()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity, enemiesHolder.transform);
+        lastSpawnTime = Time.time;
+    }
+}
