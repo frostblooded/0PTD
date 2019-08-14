1e33f9d263d069bf640d7267cec10ff42a393ec1
Nikolay Danailov <frostblooded@yahoo.com>
2019-08-14 16:41:37 +0300

    Make the maze generate paths through DFS. It should be changed to generate only one path.

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -16,14 +16,47 @@ public class Maze : MonoBehaviour
     int basePlatformZScale;
     Transform platformsHolder;
 
+    [SerializeField]
+    List<MazeUnit> borderMazeUnits;
+
     readonly float MAZE_UNIT_POSITION_OFFSET = 0.5f;
 
     private void Start()
     {
         platformsHolder = GameObject.Find("Platforms").transform;
         matrix = new List<List<MazeUnit>>();
+        borderMazeUnits = new List<MazeUnit>();
+
         SpawnBasePlatform();
         SpawnMazeUnits();
+        MakePath();
+        StartCoroutine("MakePath");
+    }
+
+    private IEnumerator MakePath()
+    {
+        int startingMazeUnitIndex = Random.Range(0, borderMazeUnits.Count);
+        MazeUnit startingMazeUnit = borderMazeUnits[startingMazeUnitIndex];
+
+        Stack<MazeUnit> stack = new Stack<MazeUnit>();
+        stack.Push(startingMazeUnit);
+
+        while(stack.Count > 0)
+        {
+            MazeUnit current = stack.Pop();
+            current.gameObject.SetActive(false);
+
+            foreach(var neighbour in current.RandomizedDirectNeighbours())
+            {
+                if(neighbour.CanBeDug())
+                {
+                    yield return new WaitForSeconds(0);
+
+                    neighbour.gameObject.SetActive(false);
+                    stack.Push(neighbour);
+                }
+            }
+        }
     }
 
     private void SpawnBasePlatform()
@@ -33,6 +66,7 @@ public class Maze : MonoBehaviour
 
         GameObject basePlatform = GameObject.CreatePrimitive(PrimitiveType.Cube);
         basePlatform.name = "Base Platform";
+        basePlatform.transform.parent = platformsHolder;
 
         Vector3 currentScale = basePlatform.transform.localScale;
         basePlatform.transform.localScale = new Vector3(basePlatformXScale, currentScale.y, basePlatformZScale);
@@ -63,22 +97,45 @@ public class Maze : MonoBehaviour
 
     private void SpawnMazeUnit(int i, int j, float currentX, float currentZ)
     {
-        GameObject mazeUnitObject = Instantiate(mazeUnitPrefab, platformsHolder);
+        GameObject mazeUnitObject = Instantiate(mazeUnitPrefab, transform);
         mazeUnitObject.transform.position = new Vector3(currentX - MAZE_UNIT_POSITION_OFFSET, 1, currentZ - MAZE_UNIT_POSITION_OFFSET);
 
         MazeUnit mazeUnit = mazeUnitObject.GetComponent<MazeUnit>();
         matrix[matrix.Count - 1].Add(mazeUnit);
 
+        if(AreBorderIndexes(i, j))
+        {
+            borderMazeUnits.Add(mazeUnit);
+        }
+
+        SetNeighbouring(mazeUnit, i, j);
+    }
+
+    private void SetNeighbouring(MazeUnit mazeUnit, int i, int j)
+    {
         if(i > 0)
         {
-            matrix[i - 1][j].bottomUnit = mazeUnit;
-            mazeUnit.topUnit = matrix[i - 1][j];
+            matrix[i - 1][j].BecomeDirectNeighbours(mazeUnit);
+
+            if(j > 0)
+            {
+                matrix[i - 1][j - 1].BecomeNeighbours(mazeUnit);
+            }
+
+            if(j < basePlatformZScale - 1)
+            {
+                matrix[i - 1][j + 1].BecomeNeighbours(mazeUnit);
+            }
         }
 
         if(j > 0)
         {
-            matrix[i][j - 1].rightUnit = mazeUnit;
-            mazeUnit.leftUnit = matrix[i][j - 1];
+            matrix[i][j - 1].BecomeDirectNeighbours(mazeUnit);
         }
+    }
+
+    private bool AreBorderIndexes(int i, int j)
+    {
+        return i == 0 || j == 0 || i == basePlatformXScale - 1 || j == basePlatformZScale - 1;
     }
 }
--- Assets/Scripts/MazeUnit.cs
+++ Assets/Scripts/MazeUnit.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class MazeUnit : MonoBehaviour
 {
-    public MazeUnit topUnit;
-    public MazeUnit rightUnit;
-    public MazeUnit bottomUnit;
-    public MazeUnit leftUnit;
+    public List<MazeUnit> neighbours;
+
+    // These are the neighbours that aren't neighbouring diagonally
+    public List<MazeUnit> directNeighbours;
+
+    public List<MazeUnit> RandomizedDirectNeighbours()
+    {
+        List<MazeUnit> list = new List<MazeUnit>(directNeighbours);
+        list.Shuffle();
+        return list;
+    }
+
+    public void BecomeDirectNeighbours(MazeUnit other)
+    {
+        directNeighbours.Add(other);
+        other.directNeighbours.Add(this);
+    }
+
+    public void BecomeNeighbours(MazeUnit other)
+    {
+        BecomeDirectNeighbours(other);
+        neighbours.Add(other);
+        other.neighbours.Add(this);
+    }
+
+    public bool CanBeDug()
+    {
+        return gameObject.activeSelf && GetInactiveNeighboursCount() <= 1;
+    }
+
+    private int GetInactiveNeighboursCount()
+    {
+        int inactiveNeighbours = 0;
+
+        foreach(var neighbour in neighbours)
+        {
+            if (!neighbour.gameObject.activeSelf) inactiveNeighbours++;
+        }
+
+        return inactiveNeighbours;
+    }
 }
