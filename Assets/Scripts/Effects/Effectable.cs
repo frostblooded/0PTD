0ecdef686c45c82f2ca3933ea4e36914b72faabb
Nikolay Danailov <frostblooded@yahoo.com>
2019-09-26 16:20:18 +0300

    Start making the effects system, so that we can have a poisoning tower.

---
+++ Assets/Scripts/Effects/Effectable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Effectable : MonoBehaviour
+{
+    private Dictionary<Effect, float> effects;
+
+    private void Start()
+    {
+        effects = new Dictionary<Effect, float>();
+    }
+
+    private void Update()
+    {
+        effects = GetNonexpiredEffects(effects);
+        
+        foreach(var effect in effects.Keys)
+        {
+            effect.Tick(gameObject);
+        }
+    }
+
+    private Dictionary<Effect, float> GetNonexpiredEffects(Dictionary<Effect, float> effects)
+    {
+        return effects.Where(pair => pair.Key.duration + pair.Value > Time.time).ToDictionary(pair => pair.Key, pair => pair.Value);
+    }
+
+    public void AddEffect(Effect effect)
+    {
+        effects.Add(effect, Time.time);
+    }
+}
---
+++ Assets/Scripts/Effects/PoisonEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonEffect : Effect
+{
+    public float damage = 2;
+    public float damageCooldown = 2;
+
+    float lastAttack;
+
+    public PoisonEffect()
+    {
+        lastAttack = Time.time;
+    }
+
+    public override void Tick(GameObject target)
+    {
+        if(lastAttack + damageCooldown <= Time.time)
+        {
+            Attackable attackable = target.GetComponent<Attackable>();
+            attackable.Damage(damage);
+            lastAttack = Time.time;
+        }
+    }
+}
